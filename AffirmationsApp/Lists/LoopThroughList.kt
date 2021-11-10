13cc9774787d0703869b8516e2e35edcdec00eb9
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2021-11-10 14:55:08 +0530

    Loops in list

---
+++ AffirmationsApp/Lists/LoopThroughList.kt
@@ -0,0 +1,18 @@
+fun main(){
+// While loop
+val guestsPerFamily = listOf(2, 4, 1, 3)
+var totalGuests = 0
+var index = 0
+while (index < guestsPerFamily.size) {
+    totalGuests += guestsPerFamily[index]
+    index++
+}
+println("Total Guest Count: $totalGuests")
+
+
+// For loop
+val names = listOf("Jessica", "Henry", "Alicia", "Jose")
+for (name in names) {
+    println(name)
+}
+}
